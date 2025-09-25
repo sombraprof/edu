eb90ff9a2bfcbb39bbd239365a5f8b03b147e10c
sombraprof <tiago.sombra@unichristus.edu.br>
2025-09-25 18:02:34 -0300

    aula 15

---
+++ src/content/courses/tdjd/lessons/lesson15.vue
@@ -0,0 +1,8 @@
+<script setup>
+import lessonData from './lesson15.json';
+import LessonRenderer from '@/components/lesson/LessonRenderer.vue';
+</script>
+
+<template>
+  <LessonRenderer :data="lessonData" />
+</template>
