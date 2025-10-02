a344e95e3492678594a01e5dc5f93f0c9e35dab7
sombraprof <tiago.sombra@unichristus.edu.br>
2025-10-02 19:57:38 -0300

    add lessons 16 and 17 tdjd (partial)

---
+++ src/content/courses/tdjd/lessons/lesson-17.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+export const meta = {
+  id: 'lesson-17',
+  title: 'Aula 17: Design Inclusivo e Acessibilidade em Jogos',
+  available: true,
+};
+</script>
+
+<script setup lang="ts">
+import LessonRenderer from '@/components/lesson/LessonRenderer.vue';
+import lessonData from './lesson-17.json';
+</script>
+
+<template>
+  <LessonRenderer :data="lessonData" />
+</template>
